3ecbeacba461f2bb8e0c94fab161e501de2b4743
Williams Estrada <williams@vauxoo.com>
2020-08-28 19:12:36 -0500

    Polybar: Configure multi monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
-MONITOR=$(polybar -m|tail -1|sed -e 's/:.*$//g')
+# MONITOR=$(polybar -m|tail -1|sed -e 's/:.*$//g')
 
 # Terminate already running bar instances
 killall -q polybar
 
 # Launch bar
-echo "---" | tee -a /tmp/polybar.log
-polybar main >>/tmp/polybar.log 2>&1 &
+# echo "---" | tee -a /tmp/polybar.log
+# polybar main >>/tmp/polybar.log 2>&1 &
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
+else
+  polybar --reload main &
+fi
